90a1bef97c459223a0655d533fd0019206a60de6
Haakam21 <haakamdeveloper@gmail.com>
2020-04-13 22:22:41 -0400

    Format

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ const app = express()
 const httpServer = http.createServer(app)
 
 app.use(express.json())
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html')
 })
 
 app.post('/', (req, res) => {
